75df8a91bff684d814ac19573d2cfac4894a1158
Mylena <elias@MacBook-Pro-de-Mylena.local>
2026-06-11 17:31:34 -0300

    segundo commit  do projeto

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ mensagem: 'API funcionando' });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log('Servidor rodando na porta 3000');
+});
